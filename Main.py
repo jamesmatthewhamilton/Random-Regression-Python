17eac491eebe1ee482ec6491dc447ebfd62f3e51
James Matthew Hamilton <j.hamilton@gatech.edu>
2020-06-26 19:19:33 -0400

    Added timing and better assertions.

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 from RandomLasso import random_lasso
 import numpy as np
+import time
 from sklearn.datasets import make_regression
 from sklearn.metrics import mean_squared_error
 
@@ -7,11 +8,14 @@ from sklearn.metrics import mean_squared_error
 def main():
     tests = 5
     rmse = np.zeros(tests)
+
     for ii in range(tests):
         x, y, ground_truth = make_regression(n_samples=500, n_features=1000 * (ii + 1),
                                              n_informative=50, coef=True)
-        weights = random_lasso(x, y)
+        weights = random_lasso(x, y, expected_sampling=80)
+        start_time = time.time()
         rmse[ii] = mean_squared_error(weights, ground_truth)
+        print("--- %s seconds ---" % (time.time() - start_time))
 
     print(rmse)
 
--- RandomLasso.py
+++ RandomLasso.py
@@ -7,6 +7,7 @@ regression techniques as the ratio of features to samples increases.
 :param x
 :param y
 :param bootstraps
+:param sample
 :param alpha
 :param box_width
 :param nfold
@@ -17,12 +18,28 @@ regression techniques as the ratio of features to samples increases.
 import numpy as np
 
 
-def random_lasso(x, y, bootstraps=None, alpha=None, box_width=None,
+def random_lasso(x, y, bootstraps=None, expected_sampling=40, alpha=[1, 1], box_width=None,
                 nfold=None, verbose=True, verbose_output=True):
+
     number_of_samples = x.shape[0]
     number_of_features = x.shape[1]
     assert number_of_samples == y.shape[0], \
-        "Error: Number of features in x is not equal to the length of y."
+        "Error: Number of features in x is not equal to the length of y.\n" + \
+        "Hint: Input for x had " + str(number_of_samples) + \
+        ", and input for y had " + str(y.shape[0]) + " samples."
+    assert not np.isnan(x).any(), \
+        "Error: The input x array contained at least one NA value."
+    assert not np.isnan(y).any(), \
+        "Error: The input x array contained at least one NA value."
+    assert bootstraps is None or expected_sampling == 40, \
+        "Error: Both bootstraps and expected_sampling were set" \
+        ", but bootstraps overwrites expected_sampling\n" \
+        "Hint: Only one of these two parameters should be set."
+
+    if bootstraps is None:
+        bootstraps = np.ceil(number_of_features / number_of_samples) * expected_sampling
+
+
 
     weights = np.zeros((number_of_features, 1))
     return weights
