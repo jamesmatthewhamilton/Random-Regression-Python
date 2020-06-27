468466b14d31c8a81c17f7a5547ae08326ecaa31
James Matthew Hamilton <j.hamilton@gatech.edu>
2020-06-27 14:10:30 -0400

    Added random selection of features and shuffling of samples.

--- RandomLasso.py
+++ RandomLasso.py
@@ -19,8 +19,7 @@ import numpy as np
 
 
 def random_lasso(x, y, bootstraps=None, expected_sampling=40, alpha=[1, 1], box_width=None,
-                nfold=None, verbose=True, verbose_output=True):
-
+                 nfold=None, verbose=True, verbose_output=True):
     number_of_samples = x.shape[0]
     number_of_features = x.shape[1]
     assert number_of_samples == y.shape[0], \
@@ -37,9 +36,24 @@ def random_lasso(x, y, bootstraps=None, expected_sampling=40, alpha=[1, 1], box_
         "Hint: Only one of these two parameters should be set."
 
     if bootstraps is None:
-        bootstraps = np.ceil(number_of_features / number_of_samples) * expected_sampling
+        bootstraps = int(np.ceil(number_of_features / number_of_samples) * expected_sampling)
 
+    all_feature_indices = np.arange(number_of_features)
+    all_sample_indices = np.arange(number_of_samples)
 
+    for ii in range(bootstraps):
+        random_features = np.random.choice(all_feature_indices, number_of_samples, replace=False)
+        shuffled_samples = np.random.choice(all_sample_indices, number_of_samples, replace=True)
+        new_x = x[:, random_features]
+        new_x = new_x[shuffled_samples, :]
+        new_y = y[shuffled_samples]
+
+    print(new_x.shape)
+    print(new_y.shape)
+    print("index features \n", random_features)
+    print("index samples \n", shuffled_samples)
+    print("random features \n", new_x)
+    print("shuffled samples \n", new_y)
 
     weights = np.zeros((number_of_features, 1))
     return weights
