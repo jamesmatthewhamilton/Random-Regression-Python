bdb855a3b0b90349ab9e76012ff715faf7311676
James Matthew Hamilton <j.hamilton@gatech.edu>
2020-06-27 17:46:31 -0400

    Refactored to reduce duplicate code.

--- Main.py
+++ Main.py
@@ -6,31 +6,34 @@ from sklearn.metrics import mean_squared_error
 from sklearn import linear_model
 
 def main():
+    # Testing Globals
     tests = 1
-    start_samples = 100
-    start_features = 1000
+    start_samples = 50
+    start_features = 100
     start_informative = 10
 
     rmse = np.zeros(tests)
+    rmse_t = np.zeros(tests)
 
     for ii in range(tests):
+        # Simulating Data
         x, y, ground_truth = make_regression(n_samples=start_samples, n_features=start_features, #* (ii + 1),
                                              n_informative=start_informative, coef=True)
         print("Ground Truth:\n", ground_truth.T)
 
+        # Testing Pure Lasso
         reg = linear_model.LassoCV().fit(x, y)
         print("Pure Lasso Prediction:\n", reg.coef_)
-        weights_t = mean_squared_error(reg.coef_, ground_truth)
-        print("Pure Lasso RME: ", weights_t)
+        rmse_t = mean_squared_error(reg.coef_, ground_truth)
+        print("Pure Lasso RME: ", rmse_t)
 
+        # Testing and Timing Random Lasso
         start_time = time.time()
         weights = random_lasso(x, y, expected_sampling=40)
         print("--- %s seconds ---" % (time.time() - start_time))
         print("Random Lasso Prediction:\n", weights)
         rmse[ii] = mean_squared_error(weights, ground_truth)
         print("Random Lasso RME:", rmse[ii])
-
-        rmse[ii] = mean_squared_error(weights, ground_truth)
 
     print(rmse)
 
--- RandomLasso.py
+++ RandomLasso.py
@@ -9,7 +9,7 @@ regression techniques as the ratio of features to samples increases.
 :param bootstraps
 :param sample
 :param alpha
-:param box_width
+:param sample_size
 :param nfold
 :param verbose
 :return: weights
@@ -19,10 +19,11 @@ import numpy as np
 from sklearn import linear_model
 
 
-def random_lasso(x, y, bootstraps=None, expected_sampling=40, alpha=[1, 1], box_width=None,
+def random_lasso(x, y, bootstraps=None, expected_sampling=40, alpha=[1, 1], sample_size=None,
                  nfold=None, verbose=True, verbose_output=True):
     number_of_samples = x.shape[0]
     number_of_features = x.shape[1]
+
     assert number_of_samples == y.shape[0], \
         "Error: Number of features in x is not equal to the length of y.\n" + \
         "Hint: Input for x had " + str(number_of_samples) + \
@@ -38,44 +39,41 @@ def random_lasso(x, y, bootstraps=None, expected_sampling=40, alpha=[1, 1], box_
 
     if bootstraps is None:
         bootstraps = int(np.ceil(number_of_features / number_of_samples) * expected_sampling)
-
-    all_feature_indices = np.arange(number_of_features)
-    all_sample_indices = np.arange(number_of_samples)
-
-    weights = np.zeros((bootstraps, number_of_features))
+    if sample_size is None:
+        sample_size = number_of_samples
 
     print(" ------ PART 1 ------ ")
-    for ii in range(bootstraps):
-        random_features = np.random.choice(all_feature_indices, number_of_samples, replace=False)
-        shuffled_samples = np.random.choice(all_sample_indices, number_of_samples, replace=True)
-        new_x = x[:, random_features]  # Valid
-        new_x = new_x[shuffled_samples, :]
-        new_y = y[shuffled_samples]  # Valid
-
-        norm_x = (new_x - np.mean(new_x, axis=0)) / np.std(new_x, axis=0)  #NV
-        norm_y = (new_y - np.mean(new_y))  # NV
-
-        reg = linear_model.LassoCV(fit_intercept=False).fit(norm_x, norm_y)  # NV
-        weights[ii, random_features] = reg.coef_  # Valid
-
+    weights = bootstrap_xy(x, y, bootstraps=bootstraps, sample_size=sample_size)
     importance_measure = np.sum(np.abs(weights), axis=0)
     probability_distribution = importance_measure / np.sum(importance_measure)
 
     print(" ------ PART 2 ------ ")
-    for ii in range(bootstraps):
-        random_features = np.random.choice(all_feature_indices, number_of_samples, replace=False,
-                                           p=probability_distribution)
-        shuffled_samples = np.random.choice(all_sample_indices, number_of_samples, replace=True)
-        new_x = x[:, random_features]  # Valid
-        new_x = new_x[shuffled_samples, :]
-        new_y = y[shuffled_samples]  # Valid
-
-        norm_x = (new_x - np.mean(new_x, axis=0)) / np.std(new_x, axis=0)  # NV
-        norm_y = (new_y - np.mean(new_y))  # NV
-
-        reg = linear_model.LassoCV(fit_intercept=False).fit(norm_x, norm_y)  # NV
-        weights[ii, random_features] = reg.coef_  # Valid
-
+    weights = bootstrap_xy(x, y, bootstraps=bootstraps, sample_size=sample_size,
+                           probabilities=probability_distribution)
     weights = np.sum(weights, axis=0) / bootstraps
 
     return weights
+
+
+def bootstrap_xy(x, y, bootstraps, sample_size, probabilities=None):
+    number_of_samples = x.shape[0]
+    number_of_features = x.shape[1]
+
+    all_feature_indices = np.arange(number_of_features)
+    all_sample_indices = np.arange(number_of_samples)
+    bootstrap_matrix = np.zeros((bootstraps, number_of_features))
+
+    for ii in range(bootstraps):
+        random_features = np.random.choice(all_feature_indices, sample_size, replace=False, p=probabilities)
+        shuffled_samples = np.random.choice(all_sample_indices, sample_size, replace=True)
+        new_x = x[:, random_features]  # Valid
+        new_x = new_x[shuffled_samples, :]  # Valid
+        new_y = y[shuffled_samples]  # Valid
+
+        norm_y = (new_y - np.mean(new_y))  # NV
+        norm_x = (new_x - np.mean(new_x, axis=0)) / np.std(new_x, axis=0)  # NV
+
+        reg = linear_model.LassoCV(fit_intercept=False).fit(norm_x, norm_y)  # NV
+        bootstrap_matrix[ii, random_features] = reg.coef_  # Valid
+
+    return bootstrap_matrix
