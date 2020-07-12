2f7386fa54b8917c0c426b31bbccab76103fa569
James Matthew Hamilton <j.hamilton@gatech.edu>
2020-07-11 23:10:13 -0400

    Added option to remove covergence warnings.

--- Main.py
+++ Main.py
@@ -8,9 +8,15 @@ from sklearn import linear_model
 def main():
     # Globals for Testing Lasso and Random Lasso.
     tests = 1
-    start_samples = 50
-    start_features = 100
-    start_informative = 10
+    start_samples = 20
+    start_features = 50
+    start_informative = 3
+
+    if True:
+        # Temporarily turning off Convergence Warnings
+        import warnings
+        from sklearn.exceptions import ConvergenceWarning
+        warnings.filterwarnings(action='ignore', category=ConvergenceWarning)
 
     rmse = np.zeros(tests)
     rmse_t = np.zeros(tests)
@@ -20,7 +26,7 @@ def main():
         X, y, ground_truth = make_regression(n_samples=start_samples, n_features=start_features, #* (ii + 1),
                                              n_informative=start_informative, coef=True)
 
-        # Sorting features their importance. Most important feature in X[:, 0].
+        # Sorting features by their importance. Most important feature in X[:, 0].
         sorted_indices = np.flip(np.argsort(ground_truth))
         ground_truth = ground_truth[sorted_indices]
         X = X[:, sorted_indices]
@@ -30,8 +36,8 @@ def main():
         # Testing Lasso
         reg = linear_model.LassoCV().fit(X, y)
         print("Lasso Prediction:\n", reg.coef_)
-        rmse_t = mean_squared_error(reg.coef_, ground_truth)
-        print("Lasso RME: ", rmse_t)
+        rmse_t[ii] = mean_squared_error(reg.coef_, ground_truth)
+        print("Lasso RME: ", rmse_t[ii])
 
         # Testing and Timing Random Lasso
         start_time = time.time()
@@ -41,7 +47,8 @@ def main():
         rmse[ii] = mean_squared_error(weights, ground_truth)
         print("Random Lasso RME:", rmse[ii])
 
-    print(rmse)
+    print("Lasso RMSE:", rmse_t)
+    print("Random Lasso RMSE:", rmse)
 
 
 if __name__ == "__main__":
