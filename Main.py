ba2ff2cfbbfe5cec03be1ad39404c873d8dbe691
James Matthew Hamilton <j.hamilton@gatech.edu>
2020-07-11 22:44:29 -0400

    Sorted X's features based on ground truth importance. [#2]

--- Main.py
+++ Main.py
@@ -17,19 +17,25 @@ def main():
 
     for ii in range(tests):
         # Simulating Data
-        x, y, ground_truth = make_regression(n_samples=start_samples, n_features=start_features, #* (ii + 1),
+        X, y, ground_truth = make_regression(n_samples=start_samples, n_features=start_features, #* (ii + 1),
                                              n_informative=start_informative, coef=True)
+
+        # Sorting features their importance. Most important feature in X[:, 0].
+        sorted_indices = np.flip(np.argsort(ground_truth))
+        ground_truth = ground_truth[sorted_indices]
+        X = X[:, sorted_indices]
+
         print("Ground Truth:\n", ground_truth.T)
 
         # Testing Lasso
-        reg = linear_model.LassoCV().fit(x, y)
+        reg = linear_model.LassoCV().fit(X, y)
         print("Lasso Prediction:\n", reg.coef_)
         rmse_t = mean_squared_error(reg.coef_, ground_truth)
         print("Lasso RME: ", rmse_t)
 
         # Testing and Timing Random Lasso
         start_time = time.time()
-        weights = random_lasso(x, y, expected_sampling=40)
+        weights = random_lasso(X, y, expected_sampling=40)
         print("--- %s seconds ---" % (time.time() - start_time))
         print("Random Lasso Prediction:\n", weights)
         rmse[ii] = mean_squared_error(weights, ground_truth)
